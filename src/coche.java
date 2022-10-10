86aa3b5f7f9fe63f23f6b7d73808ad830ed7e8df
augustoalr <augustoalr@gmail.com>
2022-10-10 11:44:48 -0400

    Ejercicio resuelto

---
+++ src/coche.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Augusto
+ */
+
+
+
+
+
+public class coche {
+    
+    public int puertas = 5;
+    
+    
+    public void aumentarPuertas(){
+        this.puertas++;
+    }
+    
+    
+    
+}
